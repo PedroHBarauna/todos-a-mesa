8e2979ca8b697118e4352d77fb19bef211a70a13
Pedro Barauna <phbarauna@unifesp.br>
2025-11-11 05:02:29 -0300

    chore: gambiarra

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   base: "/todos-a-mesa/",
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    viteStaticCopy({
+      targets: [{ src: "dist/index.html", dest: ".", rename: "404.html" }],
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
