82c53f256216930314770369b4563844602029ee
Pedro Barauna <phbarauna@unifesp.br>
2025-11-11 05:04:43 -0300

    chore: deploying again

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/todos-a-mesa/",
   plugins: [
     react(),
     mode === "development" && componentTagger(),
