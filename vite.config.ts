c9e33ae0fbe484a8b4ec89d8b027ab06a853b298
Pedro Barauna <phbarauna@unifesp.br>
2025-11-11 05:21:13 -0300

    chore: new try without 404 fallback

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: "/todos-a-mesa/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    viteStaticCopy({
-      targets: [{ src: "dist/index.html", dest: ".", rename: "404.html" }],
-    }),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
